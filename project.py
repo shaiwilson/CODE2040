0b28b22993b4f41effe306aa279da849b0ca043a
Shai Wilson <sjwilson2@usfca.edu>
2015-11-12 15:32:33 -0800

    extract registration token

---
+++ project.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import requests
+import json
+
+# pass a dictionary to the data argument
+shaidata = {"email": "sjwilson2@usfca.edu", "github": "https://github.com/shaiwilson/CODE2040.git"}
+headers = {'content-type': 'application/json', "Accept": 'application/json'}
+resp = requests.post('http://challenge.code2040.org/api/register', json = shaidata, headers = headers)
+mytoken = resp.text
+print(mytoken)
+
